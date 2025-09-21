2006e1c4f47d4dbca62dbf09ae794de396c691cc
siva <siva.rk.dbade@gmail.com>
2025-09-21 09:15:43 +0530

    changes to the shell file

--- conversation_with_variables.sh
+++ conversation_with_variables.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
 PERSON1=Siva
-PERSON2=Rekah
+PERSON2=Rekha
 
-echo "$PERSON1 ::  Hello $(PERSON2), How are you ?"
-echo "$PERSON2 :: Hello $(PERSON1)  I am fine. How are you ?"
-echo "$PERSON1 :: Where ar you currently staying $(PERSON2)"
+echo "$PERSON1 ::  Hello $PERSON2, How are you ?"
+echo "$PERSON2 :: Hello $PERSON1  I am fine. How are you ?"
+echo "$PERSON1 :: Where ar you currently staying $PERSON2"
 echo "$PERSON2 :: We are about to move to Guntur soon, $PERSON1"
